9f3661fd06808346219cee19b9ac8047c969c56b
Max Rekett <max.rekett@t2max.de>
2026-09-20 16:19:10 +0200

    fix

--- turn-off-aux-prop.lua
+++ turn-off-aux-prop.lua
@@ -1,5 +1,5 @@
 local prop1 = peripheral.wrap("Create_RotationSpeedController_13")
 local prop2 = peripheral.wrap("Create_RotationSpeedController_12")
 
-prop1.setTargetSpeed("0")
-prop2.setTargetSpeed("0")
+prop1.setTargetSpeed(0)
+prop2.setTargetSpeed(0)
